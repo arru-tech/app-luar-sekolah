c0c173a369822cadd2d7b4139caea17de2e28237
Andrei Hansel <minecraftcaptain52@gmail.com>
2024-11-17 19:50:55 +0700

    First Commit revised -andrei

---
+++ component/navbar.php
@@ -0,0 +1,19 @@
+<nav class="top-menu">
+    <div class="logo">
+        <a href="index.php">Company Logo</a>
+    </div>
+    <div class="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="login.php">Log In</a></li>
+            <li><a href="signup.php">Sign Up</a></li>
+        </ul>
+    </div>
+    <div class="search-bar">
+        <input type="text" placeholder="Search..">
+        <button type="submit">Search</button>
+    </div>
+</nav>
