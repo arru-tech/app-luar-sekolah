c6e47caec6660ba0cbf25d58cfcb403316cd7768
arru-tech <arru.zq@gmail.com>
2024-11-26 22:35:07 +0700

    add contact section

---
+++ component/contact.php
@@ -0,0 +1,13 @@
+<section class="contact-section">
+    <div class="contact-parent">
+        <div class="contact-title">
+            <p>Bingung pilih komunitas yang cocok?</p>
+        </div>
+        <div class="contact-text">
+            <p>Komunikasikan dulu dengan admin, bisa jadi solusi!</p>
+        </div>
+        <div class="contact-button">
+            <a href="">Klik untuk tanya lebih lanjut</a>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/contact.css">
 </head>
 <body>
     <?php include './component/navbar.php'; ?>
     <?php include './component/hero.php'; ?>
     <?php include './component/alumni.php'; ?>
+    <?php include './component/contact.php'; ?>
     
     <div class="content">
         
