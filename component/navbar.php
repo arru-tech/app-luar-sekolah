ff3f9261ee66d234e890b4bddbd15f5d35fcfa92
Andrei Hansel <minecraftcaptain52@gmail.com>
2024-11-29 21:38:24 +0700

    Update Navbar sesuai dengan struktur dari tim UI

--- component/navbar.php
+++ component/navbar.php
@@ -1,16 +1,40 @@
-<nav class="top-menu">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"/>
+<link rel="stylesheet" href="./css/navbar.css">
+    
+<nav class="navbar">
     <div class="logo">
-        <img src="./img/LS-logo-2nd-master.png" alt="">
-    </div>
-    <div class="search-bar">
-        <input type="text" placeholder="Search..">
-        <button type="submit">Search</button>
+        <img src="./img/LS-logo-2nd-master.png" alt="Logo">
     </div>
     <div class="menu">
         <ul>
-            <li><a href="login.php">Masuk</a></li>
-            <li><a href="signup.php" >Daftar</a></li>
+            <li><a href="#" class="nav-link home">Home</a></li>
+            <li><a href="#" class="nav-link testimoni">Testimoni</a></li>
+            <li><a href="#" class="nav-link komunitas">Komunitas</a></li>
+            <li>
+                    <a href="#" class="nav-link program">Program <i class="fa-solid fa-angle-down fa-sm" style="vertical-align: middle; margin-left: 5px;"></i></a>
+            
+                <div class="dropdown">
+                    <a href="#">Program 1</a>
+                    <a href="#">Program 2</a>
+                    <a href="#">Program 3</a>
+                </div>
+            </li>
         </ul>
     </div>
-    
+    <a href="#" class="get-started">Get Started</a>
+    <div class="hamburger" onclick="toggleMenu()">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 </nav>
+<script>
+    function toggleMenu() {
+        const menu = document.querySelector('.navbar .menu');
+        menu.classList.toggle('active');
+    }
+
+    // Set link aktif
+    document.querySelector('.nav-link.home').classList.add('active');
+</script>
+
