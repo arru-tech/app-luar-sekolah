80cf832287847ff2191a31a74704e10f6d3f90f3
arru-tech <arru.zq@gmail.com>
2024-11-30 22:40:57 +0700

    Updated footer style

--- component/footer.php
+++ component/footer.php
@@ -1,27 +1,49 @@
 <footer class="footer-container">
-    <div class=footer-col>
-        <hr>
-        <div class="footer-col-2">
-            <div class="footer-comp-1">
+    <div class="footer-col">
+        <div class="footer-row-1">
+            <div class="footer-content-1">
                 <img src="./img/LS-logo-2nd-master.png" alt="">
+                <p>Luar Sekolah adalah platform untuk belajar dan mengajar pelajaran nonformal dan formal, menyediakan kelas dalam format online dan offline</p>
             </div>
-            <div class="footer-comp-2">
-                <a href="">Home</a>
-                <a href="">About Us</a>
-                <a href="">Pricing</a>
-                <a href="">Features</a>
-                <a href="">Contact</a>
-                <a href="">Terms</a>
-                <a href="">Privacy</a>
+            <div class="footer-content-2">
+                <div class="footer-inside1">
+                    <a href="https://luarsekolah.com/tentang-kami">Tentang Kami</a>
+                    <a href="https://luarsekolah.com/article">Blog</a>
+                    <a href="https://luarsekolah.com/promo">Promo</a>
+                    <a href="https://luarsekolah.com/bantuan">Bantuan</a>
+                    <a href="https://luarsekolah.com/komunitas">Komunitas</a>
+                </div>
+                <div class="footer-inside2">
+                    <a href="https://magang.luarsekolah.com/">LuarSekolah<br>Magang+</a>
+                </div>
+                <div class="footer-inside3">
+                    <a href="https://luarsekolah.com/komunitas#">Ajukan Penawaran</a>
+                    <a href="https://luarsekolah.com/pages#terms-of-use">Syarat & Ketentuan</a>
+                    <a href="https://luarsekolah.com/pages#privacy-policy">Kebijakan Privasi</a>
+                </div>
             </div>
-            <div class="footer-comp-3">
-                <a href=""><img src="./img/iconsDribbble.png" alt=""></a>
-                <a href=""><img src="./img/iconsInstagram.png" alt=""></a>
-                <a href=""><img src="./img/iconsLinkedin.png" alt=""></a>          
+            <div class="footer-content-3">
+                <div class="footer-inside4">
+                    <a href="https://play.google.com/store/apps/details?id=com.luarsekolah.mobile">
+                        <img src="./img/logo-google-play.png" alt="">  
+                    </a>
+                </div>
+                <div class="footer-inside5">
+                    <a href="https://www.instagram.com/luarsekolah/">
+                        <img src="./img/instagram.png" alt="">
+                    </a>
+                    <a href="https://www.linkedin.com/company/luarsekolah-com">
+                        <img src="./img/LinkedIn.png" alt="">
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="footer-col-3">
-            <p>© 2024 LuarSekolah. All Rights Reserved.</p>
+        <div class="footer-row-2">
+            <hr>
+        </div>
+        <div class="footer-row-3">
+            <p>PT Teknologi Edukasi Indonesia</p>
+            <p>Copyright © 2024. All right Reserved</p>
         </div>
     </div>
 </footer>
