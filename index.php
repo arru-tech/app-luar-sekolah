f201642960319944667182a9dd3e919305b95aca
uddien <azzammataram@gmail.com>
2024-12-01 09:36:58 +0700

    update

--- index.php
+++ index.php
@@ -9,19 +9,19 @@
     <link rel="stylesheet" href="./css/hero.css">
     <link rel="stylesheet" href="./css/benefit.css">
     <link rel="stylesheet" href="./css/alumni.css">
+    <link rel="stylesheet" href="./css/class.css">
     <link rel="stylesheet" href="./css/activity.css">
     <link rel="stylesheet" href="./css/contact.css">
     <link rel="stylesheet" href="./css/footer.css">
-    <link rel="stylesheet" href="./css/class.css">
 </head>
 <body>
     <?php include './component/navbar.php'; ?>
     <?php include './component/hero.php'; ?>
     <?php include './component/benefit.php'; ?>
     <?php include './component/alumni.php'; ?>
+    <?php include './component/class.php'; ?>
     <?php include './component/activity.php'; ?>
     <?php include './component/contact.php'; ?>
     <?php include './component/footer.php'; ?>
-    <?php include './component/class.php'; ?>
 </body>
 </html>
