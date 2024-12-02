dea1c62b8a67792dae458db6dee6a164387722be
arru-tech <arru.zq@gmail.com>
2024-12-02 18:46:29 +0700

    add asset in advantage section

--- component/advantage.php
+++ component/advantage.php
@@ -5,25 +5,25 @@
             <p id="text-advantage">Komunitas Luarsekolah</p>
         </div>
         <div class="advantage-text">
-            <p>Platform Kolaborasi untuk Belajar, Berkembang, dan Berkontribusi Bersama</p>
+            <p>Platform Kolaborasi untuk Belajar, Berkembang,<br>dan Berkontribusi Bersama</p>
         </div>
     </div>
     <div class="advantage-right">
         <div class="advantage-row-1">
-            <img src="./img/Benefit.png" alt="">
-            <img src="./img/Benefit.png" alt="">
+            <img src="./img/advantage/advantage-r1.png" alt="">
+            <img src="./img/advantage/advantage-r2.png" alt="">
         </div>
         <div class="advantage-row-2">
-            <img src="./img/Benefit.png" alt="">
-            <img src="./img/Benefit.png" alt="">
+            <img src="./img/advantage/advantage-r3.png" alt="">
+            <img src="./img/advantage/advantage-r4.png" alt="">
         </div>
         <div class="advantage-row-3">
-            <img src="./img/Benefit.png" alt="">
-            <img src="./img/Benefit.png" alt="">
+            <img src="./img/advantage/advantage-r5.png" alt="">
+            <img src="./img/advantage/advantage-r6.png" alt="">
         </div>
         <div class="advantage-row-4">
-            <img src="./img/Benefit.png" alt="">
-            <img src="./img/Benefit.png" alt="">
+            <img src="./img/advantage/advantage-r7.png" alt="">
+            <img src="./img/advantage/advantage-r8.png" alt="">
         </div>
     </div>
 
