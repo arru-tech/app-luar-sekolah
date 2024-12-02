db994ae94669e28e23f492cc5c14f85cb35d953b
arru-tech <arru.zq@gmail.com>
2024-12-02 18:03:15 +0700

    add advantage section

---
+++ component/advantage.php
@@ -0,0 +1,30 @@
+<section class="advantage-container">
+    <div class="advantage-left">
+        <div class="advantage-title">
+            <p>Manfaat bergabung di</p>
+            <p id="text-advantage">Komunitas Luarsekolah</p>
+        </div>
+        <div class="advantage-text">
+            <p>Platform Kolaborasi untuk Belajar, Berkembang, dan Berkontribusi Bersama</p>
+        </div>
+    </div>
+    <div class="advantage-right">
+        <div class="advantage-row-1">
+            <img src="./img/Benefit.png" alt="">
+            <img src="./img/Benefit.png" alt="">
+        </div>
+        <div class="advantage-row-2">
+            <img src="./img/Benefit.png" alt="">
+            <img src="./img/Benefit.png" alt="">
+        </div>
+        <div class="advantage-row-3">
+            <img src="./img/Benefit.png" alt="">
+            <img src="./img/Benefit.png" alt="">
+        </div>
+        <div class="advantage-row-4">
+            <img src="./img/Benefit.png" alt="">
+            <img src="./img/Benefit.png" alt="">
+        </div>
+    </div>
+
+</section>
--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/navbar.css">
     <link rel="stylesheet" href="./css/hero.css">
-    <link rel="stylesheet" href="./css/benefit.css">
+    <link rel="stylesheet" href="./css/advantage.css">
+    <!-- <link rel="stylesheet" href="./css/benefit.css"> -->
     <link rel="stylesheet" href="./css/alumni.css">
     <link rel="stylesheet" href="./css/class.css">
     <link rel="stylesheet" href="./css/activity.css">
@@ -17,6 +18,7 @@
 <body>
     <?php include './component/navbar.php'; ?>
     <?php include './component/hero.php'; ?>
+    <?php include './component/advantage.php'; ?>
     <!-- <?php include './component/benefit.php'; ?> -->
     <?php include './component/alumni.php'; ?>
     <?php include './component/class.php'; ?>
