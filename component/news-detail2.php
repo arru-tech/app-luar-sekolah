b569e4101754e8a869c3ae93cce7a62044954898
uddien <azzammataram@gmail.com>
2024-12-04 10:38:33 +0700

    news detail 2

---
+++ component/news-detail2.php
@@ -0,0 +1,117 @@
+<section class="event-parent">
+    <div class="event-container">
+        <div class="event-container-1">
+            <p>Kegiatan Terbaru</p>
+            <div class="event-section">
+                <div class="event-section-1">
+                    <div class="card-big">
+                        <div class="card-image">
+                            <img src="../img/news_img2.png" alt="Webinar">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-date">
+                                <img src="../img/clock.png" alt="" id="no-padding">
+                                <span>Sabtu, 30/11/2024</span>
+                                <span>|</span>
+                                <!-- need to update -->
+                                <img src="../img/share-button.png" alt="">
+                                <a href="https://www.facebook.com/luarsekolahofficial/" target="_blank">
+                                    <img src="../img/facebook-button.png" alt="Facebook">
+                                </a>
+                                <a href="https://api.whatsapp.com/send?phone=628112021444&text=Halo%20Luarsekolah" target="_blank">
+                                    <img src="../img/whatsapp-button.png" alt="WhatsApp">
+                                </a>
+                                <a href="https://www.instagram.com/luarsekolah/" target="_blank">
+                                    <img src="../img/instagram-button.png" alt="Instagram">
+                                </a>
+                            </div>
+                            <h3 class="card-title">
+                                Workshop Web Dev Batch 52
+                            </h3>
+                            <div class="event-text">
+                                <p>
+                                    Batch ke-51 Workshop Web Development sukses diselenggarakan sebagai bagian dari program Magang Luar Sekolah. Program ini dirancang untuk membekali peserta dengan keterampilan teknis dan pengalaman praktis dalam dunia pengembangan web, sekaligus memberikan wawasan mendalam tentang kebutuhan industri teknologi saat ini.
+                                </p>
+                                <p>
+                                    Workshop yang berlangsung selama lima hari ini menarik minat puluhan peserta dari berbagai latar belakang pendidikan. Dengan mengusung tema "Membangun Karier Digital di Era Transformasi Teknologi," para peserta diajak untuk belajar langsung dari para ahli di bidangnya. Materi yang disampaikan meliputi dasar-dasar pengembangan web, teknologi modern seperti HTML, CSS, JavaScript, hingga pemahaman mendalam tentang kerangka kerja populer seperti React dan Node.js.
+                                </p>
+                                <p>
+                                    Tidak hanya itu, sesi praktek juga menjadi highlight dari workshop ini. Peserta dibagi ke dalam kelompok kecil untuk mengembangkan proyek mini berupa website fungsional. Dalam prosesnya, mereka tidak hanya belajar aspek teknis, tetapi juga pentingnya kolaborasi, komunikasi, dan manajemen waktu dalam menyelesaikan sebuah proyek tim.
+                                    <br>Sebagai penutup, workshop ini menghadirkan sesi diskusi interaktif bersama alumni dan mentor yang sudah berhasil meniti karier di bidang teknologi. Mereka berbagi kisah inspiratif dan memberikan kiat-kiat praktis bagi peserta untuk meraih sukses dalam karier pengembangan web.
+                                </p>
+                                <p>
+                                    “Workshop ini sangat membantu saya memahami teknologi web modern. Selain itu, pengalaman bekerja dalam tim proyek memberikan saya gambaran nyata tentang tantangan dan solusi di dunia kerja,” ujar salah satu peserta workshop.
+                                    <br>Dengan selesainya Batch 51, program Magang Luar Sekolah semakin membuktikan komitmennya dalam mempersiapkan generasi muda yang kompeten dan siap menghadapi tantangan di industri teknologi. Bagi Anda yang tertarik mengikuti batch berikutnya, pantau terus informasi terbaru di situs resmi kami!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="event-section-2">
+                    <div class="card" id="card-margin">
+                        <div class="card-image">
+                            <img src="../img/news_img3.png" alt="Webinar">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-date">
+                                <img src="../img/clock.png" alt="">
+                                <span>Sabtu, 30/11/2024</span>
+                            </div>
+                            <h3 class="card-title">
+                                Workshop Web Dev Batch 53
+                            </h3>
+                            <a href="../news/news-detail3.php" class="card-link">Read more</a>
+                        </div>
+                    </div>
+                    <div class="card" id="card-margin-2">
+                        <div class="card-image">
+                            <img src="../img/news_img4.png" alt="Webinar">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-date">
+                                <img src="../img/clock.png" alt="">
+                                <span>Sabtu, 30/11/2024</span>
+                            </div>
+                            <h3 class="card-title">
+                                Workshop Web Dev Batch 54
+                            </h3>
+                            <a href="../news/news-detail4.php" class="card-link">Read more</a>
+                        </div>
+                    </div>
+                    <div class="card" id="card-margin-2">
+                        <div class="card-image">
+                            <img src="../img/card-images.png" alt="Webinar">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-date">
+                                <img src="../img/clock.png" alt="">
+                                <span>Sabtu, 30/11/2024</span>
+                            </div>
+                            <h3 class="card-title">
+                                Workshop Web Dev Batch 50
+                            </h3>
+                            <a href="../news-detail.php" class="card-link">Read more</a>
+                        </div>
+                    </div>
+                    <div class="card" id="card-margin-2">
+                        <div class="card-image">
+                            <img src="../img/news_img1.png" alt="Webinar">
+                        </div>
+                        <div class="card-content">
+                            <div class="card-date">
+                                <img src="../img/clock.png" alt="">
+                                <span>Sabtu, 30/11/2024</span>
+                            </div>
+                            <h3 class="card-title">
+                                Workshop Web Dev Batch 51
+                            </h3>
+                            <a href="../news/news-detail1.php" class="card-link">Read more</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
