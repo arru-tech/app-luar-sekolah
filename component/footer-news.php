70e08ec7a724c5e6fa3e7c587ce7b3eda9ea2ed3
uddien <azzammataram@gmail.com>
2024-12-04 10:37:32 +0700

    footer news

---
+++ component/footer-news.php
@@ -0,0 +1,49 @@
+<footer class="footer-container">
+    <div class="footer-col">
+        <div class="footer-row-1">
+            <div class="footer-content-1">
+                <img src="../img/LS-logo-master.png" alt="">
+                <p>Luar Sekolah adalah platform untuk belajar dan mengajar pelajaran nonformal dan formal, menyediakan kelas dalam format online dan offline</p>
+            </div>
+            <div class="footer-content-2">
+                <div class="footer-inside1">
+                    <a href="https://luarsekolah.com/tentang-kami">Tentang Kami</a>
+                    <a href="https://luarsekolah.com/article">Blog</a>
+                    <a href="https://luarsekolah.com/promo">Promo</a>
+                    <a href="https://luarsekolah.com/bantuan">Bantuan</a>
+                    <a href="https://luarsekolah.com/komunitas">Komunitas</a>
+                </div>
+                <div class="footer-inside2">
+                    <a href="https://magang.luarsekolah.com/">LuarSekolah<br>Magang+</a>
+                </div>
+                <div class="footer-inside3">
+                    <a href="https://luarsekolah.com/komunitas#">Ajukan Penawaran</a>
+                    <a href="https://luarsekolah.com/pages#terms-of-use">Syarat & Ketentuan</a>
+                    <a href="https://luarsekolah.com/pages#privacy-policy">Kebijakan Privasi</a>
+                </div>
+            </div>
+            <div class="footer-content-3">
+                <div class="footer-inside4">
+                    <a href="https://play.google.com/store/apps/details?id=com.luarsekolah.mobile">
+                        <img src="../img/logo-google-play.png" alt="">  
+                    </a>
+                </div>
+                <div class="footer-inside5">
+                    <a href="https://www.instagram.com/luarsekolah/">
+                        <img src="../img/instagram.png" alt="">
+                    </a>
+                    <a href="https://www.linkedin.com/company/luarsekolah-com">
+                        <img src="../img/LinkedIn.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-row-2">
+            <hr>
+        </div>
+        <div class="footer-row-3">
+            <p>PT Teknologi Edukasi Indonesia</p>
+            <p>Copyright © 2024. All right Reserved</p>
+        </div>
+    </div>
+</footer>
