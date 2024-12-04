c6ee6ff6c24e5526cfef0b6d0b0449364f23651c
uddien <azzammataram@gmail.com>
2024-12-04 10:40:11 +0700

    news detail3

---
+++ news/news-detail3.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Luarsekolah - Berita</title>
+    <link rel="shortcut icon" href="../img/favicon-new.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/news-detail.css">
+    <link rel="stylesheet" href="../css/footer.css">
+</head>
+<body>
+    <?php include '../component/navbar-news.php'; ?>
+    <?php include '../component/news-detail3.php'; ?>
+    <?php include '../component/footer-news.php'; ?>
+</body>
+</html>
